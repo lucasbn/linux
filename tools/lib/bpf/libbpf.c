52b38438427cbb820b2208efc4c729561aef9442
lucas <lucasbnielsen@hotmail.com>
2025-04-08 16:29:20 +0000

    libbpf: recognize section "seccomp" The section is set to program type BPF_PROG_TYPE_SECCOMP with no attach type.

--- tools/lib/bpf/libbpf.c
+++ tools/lib/bpf/libbpf.c
@@ -9581,6 +9581,7 @@ static const struct bpf_sec_def section_defs[] = {
 	SEC_DEF("struct_ops.s+",	STRUCT_OPS, 0, SEC_SLEEPABLE),
 	SEC_DEF("sk_lookup",		SK_LOOKUP, BPF_SK_LOOKUP, SEC_ATTACHABLE),
 	SEC_DEF("netfilter",		NETFILTER, BPF_NETFILTER, SEC_NONE),
+	SEC_DEF("seccomp",         SECCOMP, 0, SEC_NONE),
 };
 
 int libbpf_register_prog_handler(const char *sec,
